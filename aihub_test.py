de7cc1abda6647d75871178481f72a98d50a0810
dmswl0311 <dmswl_0311@naver.com>
2021-04-01 10:36:16 +0900

    Ai hub 개체명 인식 api 코드 수정

--- aihub_test.py
+++ aihub_test.py
@@ -1,4 +1,3 @@
-
 #-*- coding:utf-8 -*-
 import urllib3
 import json
@@ -6,12 +5,16 @@ import json
 # 언어 분석 기술 문어/구어 중 한가지만 선택해 사용
 # 언어 분석 기술(구어)
 openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU_spoken"
-
 accessKey = "7f4cd7c9-f504-47b0-b9d8-66a30176c5df"
-analysisCode = "ner"
-
+# 형태소 분석 (문어/구어) : "morp",
+# 어휘의미 분석 (동음이의어 분석)(문어) : "wsd"
+# 어휘의미 분석 (다의어 분석)(문어) : "wsd_poly"
+# 개체명 인식 (문어/구어) : "ner"
+# 의존 구문 분석 (문어) : "dparse"
+# 의미역 인식 (문어) : "srl"
+analysisCode = "morp"
 #  // 언어 분석 기술(구어)
-text = "네 안녕하세요 홍길동 교숩니다"
+text=input("문장을 입력해주세요.")
 
 requestJson = {
     "access_key": accessKey,
@@ -30,5 +33,5 @@ response = http.request(
 )
 
 # print("[responseCode] " + str(response.status))
-# print("[responBody]")
+print("[responBody]")
 print(str(response.data,"utf-8"))
